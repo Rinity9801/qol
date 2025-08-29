f441574315eb9a7ae0e302bdd6ca92692a1fab6e
GitHub Action <action@github.com>
2025-08-29 18:14:57 +0000

    Initialize template repository proper
    Set package name to com.github.rinity9801.qol
    Set project name to qol
    Set modid to qol

--- src/main/java/com/github/rinity9801/qol/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/rinity9801/qol/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.rinity9801.qol.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/rinity9801/qol/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/rinity9801/qol/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.rinity9801.qol.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
